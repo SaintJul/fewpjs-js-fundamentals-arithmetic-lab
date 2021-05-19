21adb9e72c00fd81d6927e7d9a2ce1812a511692
SaintJul <favor.julius@gmail.com>
2021-05-18 22:19:34 -0400

    commit tree 78d95b7b38d72f8cb9257b0ea93ffa0eb10edaa2

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 1
+const num2 = 62
+
+ const multiply = num1 * num2
+ 
+
+
+ const random = Math.floor((Math.random() * 10) + 1)   
+    
+
+/*
+function mod(){    
+}
+
+function max(){    
+// */
